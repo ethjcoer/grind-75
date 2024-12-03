07c3453aba0a5e0d4d19fb2fd3e4e6b6d01bbb1f
ethjcoer <pudoljethro@gmail.com>
2024-12-03 19:26:28 +0800

    Time: 11 ms (75.55%), Space: 17.3 MB (29.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,27 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        # O(N log N) solution.
+        #
         # Strings are a sequence of unicode characters.
         # At least in Python, they can be sorted.
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
+
+        # O(N) solution.
+        #
+        # Two words with different lengths can't be anagrams.
+        if len(s) != len(t):
+            return False
+
+        # Utilize hashmaps to compare them in O(N) time.
+        hashmapS = {}
+        hashmapT = {}
+
+        # Iterate over both strings simultaneously.
+        for a, b in zip(s, t):
+            # The hashmaps are populated with characters as keys and their
+            # number of instances as values.
+            hashmapS[a] = 1 if a not in hashmapS else hashmapS[a] + 1
+            hashmapT[b] = 1 if b not in hashmapT else hashmapT[b] + 1
+
+        # Compare the two hashmaps to check if the strings are anagrams.
+        return hashmapS == hashmapT
